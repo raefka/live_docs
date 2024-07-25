6e175d518914a182ffa28facb1554ab3561c0de1
raefka <raefkacem20@gmail.com>
2024-07-25 14:17:52 +0100

    home page for creating documents

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CollaborativeRoom from '@/components/CollaborativeRoom'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
@@ -5,9 +6,9 @@ import React from 'react'
 
 const Document = () => {
   return (
-    <div>
-      
-    </div>
+    <main className='flex w-full flex-col items-center'>
+      <CollaborativeRoom />
+    </main>
   )
 }
 
