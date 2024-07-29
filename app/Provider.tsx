dfc90fc77893d3bbe2ee92b53e1fa2914d85f0ff
raefka <raefkacem20@gmail.com>
2024-07-29 15:30:27 +0100

    sentry integration

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,17 +1,29 @@
 "use client"
 import Loader from "@/components/Loader"
-import { getClerkUsers } from "@/lib/actions/user.actions"
+import { getClerkUsers, getDocumentsUsers } from "@/lib/actions/user.actions"
+import { useUser } from "@clerk/nextjs"
 import { LiveblocksProvider , ClientSideSuspense } from "@liveblocks/react/suspense"
 import { ReactNode } from "react"
 
 
 const Provider = ({children}:{children :ReactNode}) => {
+  const {user : clerkUser} = useUser();
   return (
     <LiveblocksProvider authEndpoint="/api/liveblocks-auth" resolveUsers={async ({userIds}) => {
       const users = await getClerkUsers({userIds});
 
       return users;
-    }}>
+    }}
+      resolveMentionSuggestions={async ({text, roomId}) =>{
+        const roomUsers = await getDocumentsUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text
+        });
+
+        return roomUsers;
+      }}
+    >
       
         <ClientSideSuspense fallback={<Loader />}>
           {children}
