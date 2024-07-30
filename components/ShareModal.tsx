780ea57076e208ea8e39ab7df60251df42d24f75
raefka <raefkacem20@gmail.com>
2024-07-30 21:02:49 +0100

    adding collaboration menu

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -14,6 +14,10 @@ import {
   } from "@/components/ui/dialog"
 import { Button } from './ui/button';
 import Image from 'next/image';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
+import UserTypeSelector from './UserTypeSelector';
+import Collaborator from './Collaborator';
   
 
 const ShareModal = ({roomId , collaborators , creatorId , currentUserType} : ShareDocumentDialogProps) => {
@@ -51,6 +55,44 @@ const ShareModal = ({roomId , collaborators , creatorId , currentUserType} : Sha
                 Select which users can view and edit this document
             </DialogDescription>
             </DialogHeader>
+
+             <Label htmlFor='email' className='mt-6 text-blue-100'>
+                Email address
+             </Label>
+             <div className='flex items-center gap-3'>
+                <div className='flex flex-1 rounded-md bg-dark-400'>
+                    <Input
+                      id="email"
+                      placeholder='Enter Email Address'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className='share-input'
+                    />
+                    <UserTypeSelector
+                       userType={userType}
+                       setUserType={setUserType}
+                    />
+                </div>
+                <Button type='submit' onClick={shareDocumentHandler} className='gradient-blue flex h-full gap-1 px-5' disabled={loading}>
+                    {loading ? 'Sending...':'Invite'}
+                </Button>
+             </div>
+
+             <div className='my-2 space-y-2'>
+                <ul className='flex flex-col'>
+                   {collaborators.map((collaborator) =>(
+                    <Collaborator
+                      key={collaborator.id}
+                      roomId={roomId}
+                      creatorId={creatorId}
+                      email={collaborator.email}
+                      collaborator={collaborator}
+                      user={user.info}
+                      
+                    />
+                   ))}
+                </ul>
+             </div>
         </DialogContent>
     </Dialog>
 
