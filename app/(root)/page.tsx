55f03664ba7afb94e11990f5c670420d967f4567
raefka <raefkacem20@gmail.com>
2024-07-31 20:13:16 +0100

    add share and delete document

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import AddDocumentBtn from '@/components/AddDocumentBtn'
+import DeleteModal from '@/components/DeleteModal'
 import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
 import { getDocuments } from '@/lib/actions/room.actions'
@@ -50,6 +51,7 @@ const Home = async () => {
                       <p className='text-sm font-light text-blue-100 '>Created about {dateConverter(createdAt)}</p>
                      </div>
                   </Link>
+                  <DeleteModal roomId={id} />
               </li>
              ))}
           </ul>
