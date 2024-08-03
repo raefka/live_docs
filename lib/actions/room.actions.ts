8a9547036deafc80e5112982bb8a0d33f32763b5
raefka <raefkacem20@gmail.com>
2024-08-03 17:59:33 +0100

    notifiction feature

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -83,25 +83,37 @@ export const getDocuments = async (email: string) =>{
 
 export const updateDocumentAccess = async ({roomId, email, userType, updatedBy} : ShareDocumentParams) =>{
     try {
-        const usersAccesses : RoomAccesses = {
-            [email]: getAccessType(userType) as AccessType,
-        };
-        
-        const room = await liveblocks.updateRoom(roomId,{
-            usersAccesses,
-        });
-
-
-        if(room){
-
+        const usersAccesses: RoomAccesses = {
+          [email]: getAccessType(userType) as AccessType,
         }
-
+    
+        const room = await liveblocks.updateRoom(roomId, { 
+          usersAccesses
+        })
+    
+        if(room) {
+          const notificationId = nanoid();
+    
+          await liveblocks.triggerInboxNotification({
+            userId: email,
+            kind: '$documentAccess',
+            subjectId: notificationId,
+            activityData: {
+              userType,
+              title: `You have been granted ${userType} access to the document by ${updatedBy.name}`,
+              updatedBy: updatedBy.name,
+              avatar: updatedBy.avatar,
+              email: updatedBy.email
+            },
+            roomId
+          })
+        }
+    
         revalidatePath(`/documents/${roomId}`);
         return parseStringify(room);
-
-    } catch (error) {
-        console.log(`Error updating document access : ${error}`);
-    }
+      } catch (error) {
+        console.log(`Error happened while updating a room access: ${error}`);
+      }
 };
 
 export const removeCollaborator = async ({roomId,email} :{roomId:string, email:string}) =>{
